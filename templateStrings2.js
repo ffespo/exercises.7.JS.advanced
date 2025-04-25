d7c3b2e4b984abb89a7b725a5af9827cb783e420
francesca <francescaesposito0200@gmail.com>
2025-04-26 00:13:11 +0200

    Svolgimento esercizi template strings

---
+++ templateStrings2.js
@@ -0,0 +1,11 @@
+//Utilizza una template string per creare una stringa su più righe 
+//che includa interruzioni di riga.
+
+const multiLineString = `
+Ciao!
+Questa è una stringa
+su più righe
+usando una template string. 
+`;
+
+console.log(multiLineString);
